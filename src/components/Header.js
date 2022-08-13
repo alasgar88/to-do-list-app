f112f0717c970bd15dab785879f87d8751189e0e
alasgar88 <85016138+alasgar88@users.noreply.github.com>
2022-08-14 01:03:25 +0400

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,13 @@
+import image from "../assets/image.png";
 import React from "react";
 
 const Header = () => {
-  return <div className='img-container'></div>;
+  return (
+    <div className='img-container'>
+      <img src={image} alt='' className='image' />
+    </div>
+  );
 };
 
 export default Header;
+
